e5fc12f388882e81702256ca4e5bd738afc6e055
Kevin Grimaldi <anthgrim@icloud.com>
2022-11-17 20:47:15 -0500

    Hotel booking api

---
+++ HotelBookingAPI/HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public HotelBookingController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // Create/Edit Hotel Bookings
+        [HttpPost]
+        public JsonResult CreateEdit(HotelBooking booking)
+        {
+            if (booking.Id == 0)
+            {
+                _context.Bookings.Add(booking);
+            } else
+            {
+                var bookingInDb = _context.Bookings.Find(booking.Id);
+
+                if (bookingInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+
+                bookingInDb = booking;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(booking));
+        }
+
+        // Get Hotel Booking
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.Bookings.Find(id);
+
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+
+            return new JsonResult(Ok(result));
+        }
+
+        // Delete Hotel Booking
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var result = _context.Bookings.Find(id);
+
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+
+            _context.Bookings.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(Ok("Deleted"));
+        }
+
+        // Get all
+        [HttpGet()]
+        public JsonResult GetAll()
+        {
+            var all = _context.Bookings.ToList();
+
+            return new JsonResult(Ok(all));
+        }
+    }
+}
